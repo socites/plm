5eaf6aca650e0666008eedf11b5a0c1255b13510
enriquebox <ebox@socites.com>
2018-05-24 08:05:40 -0300

    Initial setup

--- application/model/define.js
+++ application/model/define.js
@@ -3,12 +3,11 @@ define(['libraries/plm/main/code'], function (plm) {
     module.plm = plm;
 
     // Register Student factory
-    var factory = plm.factories.register('students', Student);
+    plm.factories.register('students', Student);
 
     return {
         'Student': Student,
-        'Students': Students,
-        'factories': factory
+        'Students': Students
     };
 
 });
