87c8f9f32dc87c168bc1085d5aa5282be51cb9f3
enriquebox <ebox@socites.com>
2018-05-25 09:20:40 -0300

    Initial setup

--- application/model/define.js
+++ application/model/define.js
@@ -3,7 +3,12 @@ define(['libraries/plm/main/code'], function (plm) {
     module.plm = plm;
 
     // Register Student factory
-    plm.registry.add('students', Student);
+    plm.registry.add('students', Student, {
+        'module': module,
+        'paths': {
+            'fetch': 'students/get'
+        }
+    });
 
     return {
         'Students': Students
--- application/model/student.js
+++ application/model/student.js
@@ -3,10 +3,6 @@ function Student(id, timeUpdated) {
     var Item = module.plm.Item;
     var item = new Item(this, id, timeUpdated);
 
-    item.module = module;
-
-    item.fetch = {'action': 'students/get'};
-
     item.setData = function (data) {
 
     };
