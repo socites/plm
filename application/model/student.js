e46ce753c44f7e4b314b99af44836ae9c2bd8f8b
enriquebox <ebox@socites.com>
2018-05-25 22:38:28 -0300

    Initial setup

--- application/model/student.js
+++ application/model/student.js
@@ -3,4 +3,10 @@ function Student(id, timeUpdated) {
     let Item = module.plm.Item;
     let item = new Item(this, id, timeUpdated);
 
+    item.update = function (data) {
+
+        console.log(data);
+
+    };
+
 }
