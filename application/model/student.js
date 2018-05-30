4c67038b29078da1c4d5a0fbd455441bd8b600da
enriquebox <ebox@socites.com>
2018-05-30 13:16:53 -0300

    Initial setup

--- application/model/student.js
+++ application/model/student.js
@@ -3,11 +3,10 @@ function Student(id) {
     let Item = module.plm.Item;
     let item = new Item(this, id);
 
-    let data = item.data
+    let data = item.data;
 
     Object.defineProperty(this, 'name', {
         'get': function () {
-            console.log(data);
             return data.name;
         }
     });
