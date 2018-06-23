9f709ddd59af4684ac1f27f91d68b3db042cbfca
enriquebox <ebox@socites.com>
2018-06-23 11:43:45 -0300

    Initial setup

--- application/pages/channel/control/js/state.js
+++ application/pages/channel/control/js/state.js
@@ -4,4 +4,6 @@ function updateState(controller, state, properties) {
     state.items = controller.channel.getters;
     properties.processing = controller.channel.fetching;
 
+    console.log('channel', state.items);
+
 }
