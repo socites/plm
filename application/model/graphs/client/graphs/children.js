7803e4b0fcb2a3e3789514ce28eed401fadd9c02
enriquebox <ebox@socites.com>
2018-06-25 12:58:05 -0300

    Initial setup

--- application/model/graphs/client/graphs/children.js
+++ application/model/graphs/client/graphs/children.js
@@ -1,6 +1,6 @@
 function GraphChildren(graph, item, entity, session) {
 
-    let children = {};
+    let children = new Map();
 
     /*
     let comments = new Graphs({'container': this, 'entity': '22'}, session, {'batch': 'container'});
@@ -18,21 +18,39 @@ function GraphChildren(graph, item, entity, session) {
 
             // Create child collection
             let specs = {'container': graph, 'entity': child.id};
-            children[child.name] = new Graphs(specs, session, {'batch': 'container'});
+            let collection = new Graphs(specs, session, {'batch': 'container'});
 
-            children[child.name].bind('change', item.triggerChange);
+            collection.bind('change', item.triggerChange);
 
+            // Expose child collection
             Object.defineProperty(graph, child.name, {
                 'get': function () {
-                    return children[child.name];
+                    return collection;
                 }
             });
+
+            // Expose child getters
+            Object.defineProperty(graph.getters, child.name, {
+                'get': function () {
+                    return collection.getters;
+                }
+            });
+
+            children.set(child.name, {'specs': child, 'collection': collection});
 
         });
 
     };
 
     this.load = function (specs) {
+
+        children.forEach(function (child, name) {
+
+            if (specs[name]) {
+                child.collection.load(specs[name]);
+            }
+
+        });
 
     };
 
