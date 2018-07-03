e1a5e08ffb0e9ad02406ddf27b3ae5f68ace3308
enriquebox <ebox@socites.com>
2018-07-03 09:01:03 -0300

    Initial setup

--- application/model/graphs/server/relations/data.js
+++ application/model/graphs/server/relations/data.js
@@ -40,11 +40,67 @@ module.exports = new function () {
         return items.has(id);
     };
 
+    /**
+     * Checks if relation from and to exists
+     *
+     * @param from {string} Graph id, the 'from' of the relation
+     * @param to {string} Graph id, the 'to' of the relation
+     */
+    function find(from, to) {
+
+        let relations = [...items.values()];
+        return relations.find(item => item.from_id === from && item.to_id === to);
+
+    }
+
     this.insert = function (fields) {
+
+        if (typeof fields.from !== 'string' || typeof fields.to !== 'string') {
+            throw new Error(`Invalid fields 'from' and/or 'to'`);
+        }
+
+        if (find(fields.from, fields.to)) {
+            throw new Error('Relation already exists');
+        }
+
+        let id = (items.size + 1).toString();
+
+        let item = {
+            'id': id,
+            'time_updated': tu,
+            'is': fields.is
+        };
+
+        items.set(id, item);
+        return item;
 
     };
 
     this.update = function (id, fields) {
+
+        if (!items.has(id)) {
+            throw new Error(`Relation "${id}" does not exist`);
+        }
+
+        let item = items.get(id);
+
+        item = Object.assign(item, {
+            'time_updated': tu,
+            'is': fields.is
+        });
+        items.set(id, item);
+
+        return item;
+
+    };
+
+    this.remove = function (id) {
+
+        if (!items.has(id)) {
+            throw new Error(`Relation "${id}" does not exist`);
+        }
+
+        items.delete(id);
 
     };
 
